d5727fa5c2c72e292551b661128d9e4f4d2e66c3
st0dxi <alanpertue@gmail.com>
2023-11-21 13:28:04 +0100

    creation de la fonction connection
    retourne l'utilisateur si les information sont bonne sinon -1

---
+++ models/UtilisateurRead.php
@@ -0,0 +1,19 @@
+<?php
+
+require $pathModels.'/PDO.php';
+
+/**
+ * return l'utilisateur si sa match sinon -1;
+ */
+function connect($mail,$mdp) {
+    $bdd = getDBc();
+    $user_query = $bdd->query("select * from UTILISATEUR where mail_util = '".$mail."';");
+    $user = $user_query->fetch(PDO::FETCH_ASSOC);
+    
+    if ($user['mdp_util'] === $mdp) {
+        $user['mdp_util'] = NULL;
+        return $user;
+    }
+    
+    return -1;
+}
