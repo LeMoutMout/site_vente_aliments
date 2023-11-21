3c65c21689a039b39c2e86d02599a67419be65dc
Klem <98971378+KlemGG@users.noreply.github.com>
2023-11-21 16:05:15 +0100

    page d'accueil avec connexion

--- connexion.js
+++ connexion.js
@@ -1,9 +1,12 @@
 function openPopup() {
-    document.getElementById('overlay').style.display = 'block';
-    document.getElementById('popup').style.display = 'block';
-  }
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("main").style.paddingTop="1000px";
+}
 
-  function closePopup() {
-    document.getElementById('overlay').style.display = 'none';
-    document.getElementById('popup').style.display = 'none';
-  }
+function closePopup() {
+    document.getElementById("main").style.paddingTop="0px";
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("popup").style.display = "none";
+}
+
