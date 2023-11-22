cee9f2af2d9104f8a28eda53852cdafb5495ba12
st0dxi <alanpertue@gmail.com>
2023-11-22 09:27:36 +0100

    add updateProduit
    changer sont id,nom,quantite,prix,bio

--- models/produitWrite.php
+++ models/produitWrite.php
@@ -12,3 +12,15 @@ function createProduct($idProductor,$nom,$quantite,$prix,$bio,$idUnite) {
         'idU' => $idUnite
     ]);
 }
+
+function updateProduit($idProduit,$nom,$quantite,$prix,$bio) {
+    $db = getDBc();
+    
+    $db->prepare('update PRODUIT set nom_produit = :nom, quantite_produit = :quant, prix_produit = :prix, bio_produit = :bio where id_produit = :idP')->execute([
+        'nom' => $nom,
+        'quant' => $quantite,
+        'prix' => $prix,
+        'bio' => $bio,
+        'idP' => $idProduit
+    ]);
+}
