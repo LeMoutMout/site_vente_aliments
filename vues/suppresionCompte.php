f25769c9e8a821f57d09cbd7e15dd8a67b374f8e
st0dxi <alanpertue@gmail.com>
2024-01-08 08:16:27 +0100

    confirmation delete

--- vues/suppresionCompte.php
+++ vues/suppresionCompte.php
@@ -1,12 +1,14 @@
 <link rel="stylesheet" href="../supressionCompte.css">
 <div class="centre_elem warp">
-    <div class="text">
-        etes-vous sur de vouloir supprimer le compte
-    </div>
-    <div class="btn oui">
-        oui
-    </div> 
-    <div class="btn non">
-        non
+    <div class="centre_elem_bloc warp bloc">
+        <div class="text">
+            etes-vous sur de vouloir supprimer le compte ?
+        </div>
+        <div class="btn non" onclick=" location.href = '?del=yes'">
+            non
+        </div>
+        <div class="btn oui">
+            oui
+        </div>
     </div>
 </div>
