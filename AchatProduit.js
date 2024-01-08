5b58000e4433d72d8600e2234e33373f1735e69d
st0dxi <alanpertue@gmail.com>
2024-01-08 18:14:34 +0100

    recherche&popup

--- AchatProduit.js
+++ AchatProduit.js
@@ -1,6 +1,6 @@
 function openPopup(id_produit, nom_produit, nom_production, stock, unite, image) {
-    document.getElementById("overlay").style.display = "block";
-    document.getElementById("popup").style.display = "block";
+    document.getElementById("overlay_achat").style.display = "block";
+    document.getElementById("popup_achat").style.display = "block";
   
     var form = document.querySelector('form');
     form.reset();
@@ -20,6 +20,6 @@ function openPopup(id_produit, nom_produit, nom_production, stock, unite, image)
   }
   
   function closePopup() {
-    document.getElementById("overlay").style.display = "none";
-    document.getElementById("popup").style.display = "none";
+    document.getElementById("overlay_achat").style.display = "none";
+    document.getElementById("popup_achat").style.display = "none";
   }
